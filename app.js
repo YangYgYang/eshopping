e4602079dd5f063eebec4af802b3419fc6d3f3e4
YangYgYang <you@example.com>
2022-10-21 20:21:41 +0800

    run the server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000 || process.env.port
+
+app.get('/',(req,res)=>{
+    res.send('server is running')
+})
+
+
+app.listen(port, () => {
+    console.info(`Example app listening on port ${port}!Let's go to http://localhost:3000`)
+  })
