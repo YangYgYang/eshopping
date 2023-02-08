3002d78b4f59f34c8c789c2a41849d463e40c485
YangYgYang <you@example.com>
2023-02-08 22:29:56 +0800

    add category seed

---
+++ seeders/20230208133650-categories-main-seed-file.js
@@ -0,0 +1,79 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    //sort 要怎麼排序，有main和branch，排序是他可以排是第幾個，但我從資料庫可以抓回來降冪排列就好
+  const categoriesSeed =[
+    {
+      name: '保養系列',
+			img: null,
+			parent_id: 0,
+			sort: 1,
+			created_at: new Date(),
+			updated_at: new Date()
+    },
+    {
+      name: '彩妝係列',
+			img: null,
+			parent_id: 0,
+			sort: 2,
+			created_at: new Date(),
+			updated_at: new Date()
+    },
+    {
+      name: '香氛係列',
+			img: null,
+			parent_id: 0,
+			sort: 3,
+			created_at: new Date(),
+			updated_at: new Date()
+    },
+    {
+      name: '身體髮品',
+			img: null,
+			parent_id: 0,
+			sort: 4,
+			created_at: new Date(),
+			updated_at: new Date()
+    },
+    {
+      name: '美妝工具',
+			img: null,
+			parent_id: 0,
+			sort: 5,
+			created_at: new Date(),
+			updated_at: new Date()
+    },
+    {
+      name: '男士保養',
+			img: null,
+			parent_id: 0,
+			sort: 6,
+			created_at: new Date(),
+			updated_at: new Date()
+    },
+  ]
+  // const categoriesName = ['保養係列','彩妝系列','香氛系列','身體髮品','美妝工具','男士保養']
+  //   const categoriesSeed = Array.from({length:6},async ()=>({
+  //     name: categoriesName,
+	// 		img: null,
+	// 		parent_id: 0,
+	// 		sort: 'root',
+	// 		created_at: new Date(),
+	// 		updated_at: new Date()
+  // }))
+		await queryInterface.bulkInsert('categories', categoriesSeed)
+  },
+
+  down: async(queryInterface, Sequelize) => {
+		const { sequelize } = queryInterface
+		try {
+			await sequelize.transaction(async transaction => {
+				const options = { transaction }
+				await sequelize.query('TRUNCATE TABLE categories', options)
+			})
+		} catch (error) {
+			console.log(error)
+		}
+  }
+};
---
+++ seeders/20230208141403-categories-branch-seed-file.js
@@ -0,0 +1,68 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const categoriesSeed =[
+      {
+        name: '乳液',
+        img: null,
+        parent_id: 0,
+        sort: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: '粉底液',
+        img: null,
+        parent_id: 1,
+        sort: 2,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: '擴香瓶',
+        img: null,
+        parent_id: 2,
+        sort: 3,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: '沐浴乳',
+        img: null,
+        parent_id: 3,
+        sort: 4,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: '各式刷具',
+        img: null,
+        parent_id: 4,
+        sort: 5,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: '控油洗面乳',
+        img: null,
+        parent_id: 5,
+        sort: 6,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+    ]
+  },
+
+  down: async(queryInterface, Sequelize) => {
+		const { sequelize } = queryInterface
+		try {
+			await sequelize.transaction(async transaction => {
+				const options = { transaction }
+				await sequelize.query('TRUNCATE TABLE categories', options)
+			})
+		} catch (error) {
+			console.log(error)
+		}
+  }
+};
