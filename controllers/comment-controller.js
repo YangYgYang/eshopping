f8bc373075c67f8f9fd670f7edb891b4fda4a79d
YangYgYang <you@example.com>
2023-02-23 23:56:33 +0800

    fix get product comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,12 +3,23 @@ const { Products,Categories, Comments} = require('../models')
 
 const commentController = {
     getComments: async (req, res, next) =>{
-        const productId = req.query.product_id
-        const productComment = await Comments.findAll({
-            nest: true,
-            raw: true,
-            where:{ product_id:productId }
-        })
+        const productId = req.query.product_id || null
+        const userId = req.query.user_id || null
+        let productComment
+        if(productId !== null){
+            productComment = await Comments.findAll({
+                nest: true,
+                raw: true,
+                where:{ product_id:productId }
+            })
+        }else if (userId !== null){
+            productComment = await Comments.findAll({
+                nest: true,
+                raw: true,
+                where:{ user_id:userId }
+            })
+        }
+
         res.status(200).json(
             productComment
         )
